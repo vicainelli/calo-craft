3a7a40edb78b53ed27b5ca41a0646036ecfe4c9a
Vinicius Cainelli <vinicius.cainelli@ingka.ikea.com>
2025-07-28 18:33:55 +0200

    feat(web): add shadcn

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
-  return <h1>CaloCraft</h1>;
-}
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
 
-export default App;
+export function App() {
+  return <>
+    <h1 className="text-3xl font-bold text-gray-600">CaloCraft</h1>
+
+    <p>Let'start</p>
+
+    <div>
+      <form>
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-1">
+            <label htmlFor="daily_calories">Amount of calories per day</label>
+            <Input type="number" name="daily_calories" id="daily_calories" placeholder="1400" />
+          </div>
+          <div>
+            <Button>confirm</Button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </>;
+}
