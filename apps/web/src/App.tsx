772f90e2513e788a3bf56ce99e8a31dac48e8aa8
Vinicius Cainelli <vinicius.cainelli@ingka.ikea.com>
2025-08-05 21:20:47 +0200

    chore: add more components

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,42 +1,73 @@
+import { z } from 'zod';
+import { useForm } from '@tanstack/react-form'
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { store } from "@/lib/store";
-import { useStore } from "@tanstack/react-store";
-
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Textarea } from "@/components/ui/textarea";
 
 export function App() {
+  return (
+    <div className="border p-8">
+      <h1 className="text-3xl font-bold text-gray-600">CaloCraft</h1>
 
-const daily_calories = useStore(store, (state) => state.daily_calories);
-    const setDailyCalories = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      const formData = new FormData(e.currentTarget);
-      const calories = formData.get("daily_calories");
-      if (typeof calories === "string") {
-        store.setState({ daily_calories: parseInt(calories, 10) });
-      }
-    };
+      <p>Let'start</p>
 
-  return <>
-    <h1 className="text-3xl font-bold text-gray-600">CaloCraft</h1>
-
-    <p>Let'start</p>
-
-    <div>
-      {daily_calories <= 0 ? (<form onSubmit={setDailyCalories}>
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-col gap-1">
-            <label htmlFor="daily_calories">Amount of calories per day</label>
-            <Input type="number" name="daily_calories" id="daily_calories" placeholder="1400" />
+      <div>
+        <form>
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="daily_calories">Amount of calories per day</label>
+              <Input
+                type="number"
+                name="daily_calories"
+                id="daily_calories"
+                placeholder="1400"
+              />
+            </div>
+            <div>
+              <label htmlFor="daily_meals">Daily meals</label>
+              <Select name="daily_meals">
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Daily meals" />
+                </SelectTrigger>
+                <SelectContent id="daily_meals">
+                  <SelectItem value="1">1</SelectItem>
+                  <SelectItem value="2">2</SelectItem>
+                  <SelectItem value="3">3</SelectItem>
+                  <SelectItem value="4">4</SelectItem>
+                  <SelectItem value="5">5</SelectItem>
+                  <SelectItem value="6">6</SelectItem>
+                  <SelectItem value="7">7</SelectItem>
+                  <SelectItem value="8">8</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div>
+              <label htmlFor="preferences_and_allergies">Preferences and allergies</label>
+              <Textarea id="preferences_and_allergies"></Textarea>
+              <p className="text-sm">Describe as much as possible your food preferences, restrictions, and allergies</p>
+            </div>
+            <div>
+              <Button type="submit">confirm</Button>
+            </div>
           </div>
-          <div>
-            <Button type="submit">confirm</Button>
-          </div>
-        </div>
-      </form>): (
-        <div>
-         Your daily calories goal is: {daily_calories}. <Button variant="ghost" onClick={() => store.setState({ daily_calories: 0 })}>Reset</Button>
-        </div>
-      )}
+        </form>
+      </div>
     </div>
-  </>;
+  );
 }
